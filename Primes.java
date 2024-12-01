79a7e3b191fc2d4badb27e577539e6554123ac33
pazrachman <paz.rachman@post.runi.ac.il>
2024-12-01 18:50:13 +0200

    ee

--- Primes.java
+++ Primes.java
@@ -1,5 +1,10 @@
 public class Primes {
     public static void main(String[] args) {
         // Replace this statement with your code
+        int [] numbers = new int [];
+
+
     }
+
+    public static int Primes (int numbers, )
 }
