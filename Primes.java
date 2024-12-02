1eb28701fe72718c15ebcf4e0464ac7bf656cdc4
pazrachman <paz.rachman@post.runi.ac.il>
2024-12-02 23:09:23 +0200

    pu

--- Primes.java
+++ Primes.java
@@ -21,13 +21,13 @@ public class Primes {
         for (int i = 2; i < isPrime.length; i++) {
             if (isPrime[i]) {
                 count++;
-                System.out.println(" " + i);
+                System.out.println(i);
             }
 
         }
 
         precent = (int) ((double) count / n * 100);
-        System.out.println("There are " + count + " primes between 2 and " + n + ".(" + precent + "% are primes)");
+        System.out.println("There are " + count + " primes between 2 and " + n + " (" + precent + "% are primes)");
     }
 
 }
